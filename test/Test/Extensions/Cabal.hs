e6c8a0eb57c10a0b6a1927097040328c5795ae6c
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-04-26 21:36:12 +0100

    [#24] Return OnOffExtension in cabal parsing
    Resolves #24

--- test/Test/Extensions/Cabal.hs
+++ test/Test/Extensions/Cabal.hs
@@ -6,6 +6,7 @@ import GHC.LanguageExtensions.Type (Extension (..))
 import Test.Hspec (Spec, describe, it, runIO, shouldBe)
 
 import Extensions.Cabal (parseCabalExtensions)
+import Extensions.OnOff (OnOffExtension (..))
 
 import qualified Data.HashMap.Strict as HM
 
@@ -17,11 +18,11 @@ cabalSpec = describe "Cabal file Extensions Parser" $ do
     it "should parse project Cabal file" $
         extensionsMap `shouldBe` expectedMap
   where
-    expectedMap :: HM.HashMap FilePath [Extension]
+    expectedMap :: HM.HashMap FilePath [OnOffExtension]
     expectedMap = HM.fromList
         [ "src/Extensions.hs"              `to` defaultExtensions
         , "src/Extensions/Cabal.hs"        `to` defaultExtensions
-        , "src/Extensions/OnOff.hs"       `to` defaultExtensions
+        , "src/Extensions/OnOff.hs"        `to` defaultExtensions
         , "src/Extensions/Parser.hs"       `to` defaultExtensions
         , "test/Test/Extensions/Cabal.hs"  `to` defaultExtensions
         , "test/Test/Extensions/OnOff.hs"  `to` defaultExtensions
@@ -31,8 +32,8 @@ cabalSpec = describe "Cabal file Extensions Parser" $ do
       where
         to = (,)
 
-    defaultExtensions :: [Extension]
-    defaultExtensions =
+    defaultExtensions :: [OnOffExtension]
+    defaultExtensions = map On
         [ ConstraintKinds
         , DeriveGeneric
         , DerivingStrategies
