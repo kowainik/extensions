58ca0b900d70aa683293eafd432329ec7d55e737
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-04-26 15:47:29 +0100

    [#4] Add tests for .cabal parsing (#20)
    Resolves #4

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,8 +2,11 @@ module Main (main) where
 
 import Test.Hspec (hspec)
 
+import Test.Extensions.Cabal (cabalSpec)
 import Test.Extensions.Parser (parserSpec)
 
 
 main :: IO ()
-main = hspec parserSpec
+main = hspec $ do
+    cabalSpec
+    parserSpec
