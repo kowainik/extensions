7a0658ce114ac8a5bf03987a96b22b8c06032053
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-05-03 17:16:07 +0100

    [#58] SafeHaskellExtensions tests (#59)
    Resolves #58

--- test/Test/Extensions/Cabal.hs
+++ test/Test/Extensions/Cabal.hs
@@ -4,12 +4,14 @@ module Test.Extensions.Cabal
     ) where
 
 import Data.ByteString (ByteString)
+import Data.List.NonEmpty (NonEmpty (..))
 import Data.Map.Strict (Map)
 import GHC.LanguageExtensions.Type (Extension (..))
 import Test.Hspec (Spec, describe, it, shouldBe, shouldThrow)
 
 import Extensions.Cabal (CabalException (..), parseCabalExtensions, parseCabalFileExtensions)
-import Extensions.Types (OnOffExtension (..), ParsedExtensions (..), emptyParsedExtensions)
+import Extensions.Types (OnOffExtension (..), ParsedExtensions (..), SafeHaskellExtension (..),
+                         emptyParsedExtensions)
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as Text
@@ -18,12 +20,15 @@ import qualified Data.Text.Encoding as Text
 
 cabalSpec :: Spec
 cabalSpec = describe "Cabal file Extensions Parser" $ do
-    it "should throw 'CabalFileNotFound' on non-existing file" $
+    it "should throw the 'CabalFileNotFound' exception on non-existing file" $
         parseCabalFileExtensions "xno-extensions.cabal" `shouldThrow`
             (== CabalFileNotFound "xno-extensions.cabal")
-    it "should throw parse error" $
+    it "should throw 'CabalParseError'" $
         parseCabalExtensions exampleCabal "stack.yaml" `shouldThrow`
             (== CabalParseError "example.cabal:0:0: \"name\" field missing")
+    it "should throw 'CabalSafeExtensionsConflict'" $
+        parseCabalExtensions exampleCabal conflictSafeCabal `shouldThrow`
+            (== CabalSafeExtensionsConflict (Safe :| [Unsafe]))
     it "should parse minimal Cabal file" $
         parseCabalExtensions exampleCabal minimalCabal >>= \extMap ->
             extMap `shouldBe` mempty
@@ -39,10 +44,19 @@ cabalSpec = describe "Cabal file Extensions Parser" $ do
     it "should parse Cabal file with default-extensions inside common stanza" $
         parseCabalExtensions exampleCabal commonStanzaCabal >>= \extMap ->
             extMap `shouldBe` singleExtensionMap
+    it "should parse Cabal file with Safe in default-extensions" $
+        parseCabalExtensions exampleCabal safeHaskellCabal >>= \extMap ->
+            extMap `shouldBe` safeExtensionMap
+    it "should parse Cabal file with different safe extensions in different stanzas" $
+        parseCabalExtensions exampleCabal manySafeCabal >>= \extMap ->
+            extMap `shouldBe` manySafeMap
     it "should parse extensions.cabal" $
         parseCabalFileExtensions "extensions.cabal" >>= \extMap ->
             extMap `shouldBe` expectedMap
   where
+    to :: FilePath -> ParsedExtensions -> (FilePath, ParsedExtensions)
+    to = (,)
+
     exampleCabal :: FilePath
     exampleCabal = "example.cabal"
 
@@ -53,6 +67,21 @@ cabalSpec = describe "Cabal file Extensions Parser" $ do
     singleExtensionMap = Map.singleton "src/Extensions.hs" emptyParsedExtensions
         { parsedExtensionsAll = [On TypeApplications]
         }
+
+    safeExtensionMap :: Map FilePath ParsedExtensions
+    safeExtensionMap = Map.singleton "src/Extensions.hs" emptyParsedExtensions
+        { parsedExtensionsSafe = Just Safe
+        }
+
+    manySafeMap :: Map FilePath ParsedExtensions
+    manySafeMap = Map.fromList
+        [ "src/Extensions.hs" `to` emptyParsedExtensions
+            { parsedExtensionsSafe = Just Safe
+            }
+        , "test/Spec.hs" `to` emptyParsedExtensions
+            { parsedExtensionsSafe = Just Trustworthy
+            }
+        ]
 
     -- Map for the project itself
     expectedMap :: Map FilePath ParsedExtensions
@@ -69,8 +98,6 @@ cabalSpec = describe "Cabal file Extensions Parser" $ do
         , "test/Test/Extensions/Parser.hs" `to` defaultExtensions
         , "test/Spec.hs"                   `to` defaultExtensions
         ]
-      where
-        to = (,)
 
     -- Minimal cabal file with no modules and no extensions
     minimalCabal :: ByteString
@@ -133,6 +160,51 @@ cabalSpec = describe "Cabal file Extensions Parser" $ do
         , "  import: common-extensions"
         , "  hs-source-dirs: src"
         , "  exposed-modules: Extensions"
+        ]
+
+    -- Cabal file with safe haskell extension
+    safeHaskellCabal :: ByteString
+    safeHaskellCabal = Text.encodeUtf8 $ Text.unlines
+        [ "cabal-version: 2.4"
+        , "name: example"
+        , "version: 0.0.0.0"
+        , ""
+        , "library"
+        , "  hs-source-dirs: src"
+        , "  exposed-modules: Extensions"
+        , "  default-extensions: Safe"
+        ]
+
+    -- Cabal file with conflicting safe haskell extensions
+    conflictSafeCabal :: ByteString
+    conflictSafeCabal = Text.encodeUtf8 $ Text.unlines
+        [ "cabal-version: 2.4"
+        , "name: example"
+        , "version: 0.0.0.0"
+        , ""
+        , "library"
+        , "  hs-source-dirs: src"
+        , "  exposed-modules: Extensions"
+        , "  default-extensions: Safe, Unsafe"
+        ]
+
+    -- Cabal file with different Safe extensions
+    manySafeCabal :: ByteString
+    manySafeCabal = Text.encodeUtf8 $ Text.unlines
+        [ "cabal-version: 2.4"
+        , "name: example"
+        , "version: 0.0.0.0"
+        , ""
+        , "library"
+        , "  hs-source-dirs: src"
+        , "  exposed-modules: Extensions"
+        , "  default-extensions: Safe"
+        , ""
+        , "test-suite example-test"
+        , "  type: exitcode-stdio-1.0"
+        , "  hs-source-dirs: test"
+        , "  main-is: Spec.hs"
+        , "  default-extensions: Trustworthy"
         ]
 
 defaultExtensions :: ParsedExtensions
